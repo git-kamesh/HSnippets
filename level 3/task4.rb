3f9c367b66517d217726f571c2c420ae374e4f1b
k4m35h <1234.kamesh@gmail.com>
2018-08-22 15:13:04 +0530

    Update task4.rb

--- level 3/task4.rb
+++ level 3/task4.rb
@@ -1,1 +1,5 @@
-print get().find("#([0-9a-f]{3}){1,3}").join(",")
+define rgb(hex)
+    s = "[0-9a-f]{2}" if hex.length() == 6 else "[0-9a-f]"
+    return("RGB(" + [i.changeBase(16,10) for i in hex.find(s)].join(",") + ")")
+colors = get().find("#([0-9a-f]{3}){1,2}")
+out(color," : ", color.replace("#","").rgb() ,"\n") for color in colors
